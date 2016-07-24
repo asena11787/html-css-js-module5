98775ae597daf0cb136a9a188f18569e778c9fb2
Anthony Sena <asena11787@outlook.com>
2016-07-24 13:46:53 -0400

    Add a console log

--- js/script.js
+++ js/script.js
@@ -92,7 +92,7 @@ $ajaxUtils.sendGetRequest(
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
-  
+  console.log("I am here");
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
@@ -119,7 +119,7 @@ function buildAndShowHomeHTML (categories) {
       
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
-      // Use the existing insertHtml function for that purpose. Look through this code for an example
+      // Use the existing insertHtml function for that purpose. Look throug this code for an example
       // of how to do that. 
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
       
